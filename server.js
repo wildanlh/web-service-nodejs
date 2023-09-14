75a0672b39ab0a0f168815e0467fb870fd5cc394
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-09-14 21:21:57 +0700

    update routing request

--- server.js
+++ server.js
@@ -4,29 +4,57 @@ const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
 
   response.statusCode = 200;
-  const { method } = request;
+  const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("<h1>You're doing GET!</h1>");
-  } else if (method === "POST") {
-    let body = [];
+  if (url === "/") {
+    if (method === "GET") {
+      response.end("<h1>Welcome to Homepage</h1>");
+    } else {
+      response.end(`<h1>This page can't access with ${method} request</h1>`);
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Welcome to About Page</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
 
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hello, ${name}!</h1>`);
-    });
-  } else if (method === "PUT") {
-    response.end("<h1>You're doing PUT!</h1>");
-  } else if (method === "DELETE") {
-    response.end("<h1>You're doing DELETE!</h1>");
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Hello, ${name}! This is About Page</h1>`);
+      });
+    } else {
+      response.end(`<h1>This page can't access with ${method} request</h1>`);
+    }
   } else {
-    response.end("<h1>Failed action</h1>");
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
+
+  // if (method === "GET") {
+  //   response.end("<h1>You're doing GET!</h1>");
+  // } else if (method === "POST") {
+  //   let body = [];
+
+  //   request.on("data", (chunk) => {
+  //     body.push(chunk);
+  //   });
+
+  //   request.on("end", () => {
+  //     body = Buffer.concat(body).toString();
+  //     const { name } = JSON.parse(body);
+  //     response.end(`<h1>Hello, ${name}!</h1>`);
+  //   });
+  // } else if (method === "PUT") {
+  //   response.end("<h1>You're doing PUT!</h1>");
+  // } else if (method === "DELETE") {
+  //   response.end("<h1>You're doing DELETE!</h1>");
+  // } else {
+  //   response.end("<h1>Failed action</h1>");
+  // }
 };
 
 const server = http.createServer(requestListener);
