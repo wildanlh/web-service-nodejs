75eee9d430f22b8fe25b32dd2f6f92b8ea6284a6
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-09-14 20:59:36 +0700

    update method request

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
 
   response.statusCode = 200;
-  response.end("<h1>Halo HTTP Server!</h1>");
+  const { method } = request;
+
+  if (method === "GET") {
+    response.end("<h1>You're doing GET!</h1>");
+  } else if (method === "POST") {
+    response.end("<h1>You're doing POST!</h1>");
+  } else if (method === "PUT") {
+    response.end("<h1>You're doing PUT!</h1>");
+  } else if (method === "DELETE") {
+    response.end("<h1>You're doing DELETE!</h1>");
+  } else {
+    response.end("<h1>Failed action</h1>");
+  }
 };
 
 const server = http.createServer(requestListener);
